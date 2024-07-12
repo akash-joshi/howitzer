d4d6d1a3ab8caf89e6171c4d94df1df9cce9d246
Work <akash.joshi@deepl.com>
2024-07-12 14:59:02 +0100

    feat: allow users to configure model

---
+++ src/config.mjs
@@ -0,0 +1,47 @@
+import inquirer from "inquirer";
+
+/** @typedef {import("conf").default} Conf */
+
+export const configurator = (/** @type {{ config: Conf }} */ { config }) => {
+  const apiKey = config.get("apiKey");
+  const model = config.get("model");
+
+  const apiKeyCopy = `API Key${apiKey ? " (Already set)" : ""}`;
+  const modelCopy = `Model${model ? ` (${model})` : ""}`;
+
+  /** @type {import("openai").OpenAI.ChatModel[]} */
+  const modelChoices = ["gpt-3.5-turbo", "gpt-4-turbo", "gpt-4o"];
+
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "action",
+        message: "What would you like to configure?",
+        choices: [apiKeyCopy, modelCopy],
+      },
+      {
+        type: "input",
+        name: "apiKey",
+        message: "Enter your new API key (Press enter to keep unchanged):",
+        when: (answers) => answers.action.includes("API Key"),
+      },
+      {
+        type: "list",
+        name: "model",
+        message: "Select the model:",
+        choices: modelChoices,
+        when: (answers) => answers.action.includes("Model"),
+      },
+    ])
+    .then((answers) => {
+      if (answers.apiKey) {
+        config.set("apiKey", answers.apiKey);
+        console.log("API key updated successfully.");
+      }
+      if (answers.model) {
+        config.set("model", answers.model);
+        console.log("Model updated successfully.");
+      }
+    });
+};
--- src/index.mjs
+++ src/index.mjs
@@ -1,97 +1,110 @@
-import { Command } from 'commander';
-import { exec } from 'child_process';
-import OpenAI from 'openai';
-import ora from 'ora';
-import inquirer from 'inquirer';
-import Conf from 'conf';
-import fs from 'fs';
-import { generateMainMessage } from './message.mjs';
-import { fileURLToPath } from 'url';
-import path from 'path';
+import { Command } from "commander";
+import { exec } from "child_process";
+import OpenAI from "openai";
+import ora from "ora";
+import inquirer from "inquirer";
+import Conf from "conf";
+import fs from "fs";
+import { generateMainMessage } from "./message.mjs";
+import { fileURLToPath } from "url";
+import path from "path";
+import { configurator } from "./config.mjs";
 
 export const cli = () => {
-
   // Convert the URL to a file path
   const __filename = fileURLToPath(import.meta.url);
   // Get the directory name of the current module
   const __dirname = path.dirname(__filename);
 
   // Correctly construct the path to package.json
-  const packageJsonPath = path.join(__dirname, '..', 'package.json');
-  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+  const packageJsonPath = path.join(__dirname, "..", "package.json");
+  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
 
-  const config = new Conf({ projectName: 'how' });
+  const config = new Conf({ projectName: "how" });
 
   const program = new Command();
 
   /** @typedef {import("openai").OpenAI.ChatCompletionMessageParam} ChatCompletionMessageParam */
 
-  program
-    .version(packageJson.version)
-    .description(packageJson.description)
+  program.version(packageJson.version).description(packageJson.description);
 
   program
     .option("-d, --debug", "log debug data")
-    .option("-c, --config", "configure API key")
-    .argument('[query]', "Enter your query in plain text. This will be used to generate a CLI command.")
+    .option("-c, --config", "configure API key and model")
+    .argument(
+      "[query]",
+      "Enter your query in plain text. This will be used to generate a CLI command."
+    )
     .action(async () => {
       const options = program.opts();
 
       if (options.debug) {
-        return console.log({ shell: process.env.SHELL, platform: process.platform })
+        return console.log({
+          shell: process.env.SHELL,
+          platform: process.platform,
+        });
       }
 
       /**@type {string|undefined} */
       // @ts-ignore - Run-time type assertion is overkill.
-      const configKey = config.get('apiKey')
+      const configKey = config.get("apiKey");
 
       /**@type {string|undefined} */
-      let apiKey = configKey ?? process.env.OPENAI_API_KEY
+      let apiKey = configKey ?? process.env.OPENAI_API_KEY;
 
-      let response
-      if (!apiKey || options.config) {
-        const MESSAGE = `${apiKey ? '' : "Missing OpenAI API Key. "}You can create or find your OpenAI API key at https://platform.openai.com/account/api-keys.`
-        console.log(MESSAGE)
-        response = await inquirer.prompt([{
-          type: 'input',
-          name: 'apiKey',
-          message: 'Paste your API key here:'
-        }]);
+      let response;
 
-        try {
-          const testClient = new OpenAI({ apiKey: response.apiKey })
+      if (options.config) {
+        return configurator({ config });
+      }
 
-          /** @type ChatCompletionMessageParam[] */
-          const messages = [{ role: 'user', content: 'Respond with pong. Ping' }]
+      /** @type {(import("openai").OpenAI.ChatModel)} */
+      let model =
+        /** @type {import("openai").OpenAI.ChatModel} */ (
+          config.get("model")
+        ) ?? "gpt-3.5-turbo";
 
-          await testClient.chat.completions.create({
-            messages,
-            model: 'gpt-3.5-turbo-0125',
-          });
-        } catch (error) {
-          if (error instanceof Error) console.log(error.message)
-          return;
-        }
+      if (!apiKey) {
+        const MESSAGE = `${
+          apiKey ? "" : "Missing OpenAI API Key. "
+        }You can create or find your OpenAI API key at https://platform.openai.com/account/api-keys.`;
+        console.log(MESSAGE);
+        response = await inquirer.prompt([
+          {
+            type: "input",
+            name: "apiKey",
+            message: "Paste your API key here:",
+          },
+        ]);
 
-        config.set('apiKey', response.apiKey)
-        apiKey = response.apiKey
+        config.set("apiKey", response.apiKey);
+        apiKey = response.apiKey;
       }
 
       const openai = new OpenAI({ apiKey });
 
+      const availableModels = await openai.models.list();
+
+      if (!availableModels.data.some((m) => m.id === model)) {
+        console.log(
+          `Model ${model} is not available for your user. Downgrading to gpt-3.5-turbo instead.`
+        );
+        model = "gpt-3.5-turbo";
+      }
+
       const query = process.argv.slice(1, process.argv.length).join(" ");
       if (!query) {
-        console.error("error: missing required argument 'query'")
+        console.error("error: missing required argument 'query'");
       }
       const messages = generateMainMessage(query);
 
-      const spinner = ora('Executing Magic ✨').start();
+      const spinner = ora("Executing Magic ✨").start();
 
       let completion;
       try {
         completion = await openai.chat.completions.create({
           messages,
-          model: 'gpt-3.5-turbo-0125',
+          model,
           response_format: { type: "json_object" },
         });
       } catch (error) {
@@ -105,9 +118,9 @@ export const cli = () => {
       messages.push(completion.choices[0].message);
       const { message } = completion.choices[0];
 
-      /** @typedef {Object} Output 
-       *  @property {string} command 
-       *  @property {string} explanation 
+      /** @typedef {Object} Output
+       *  @property {string} command
+       *  @property {string} explanation
        */
       /** @type {Output} */
       const output = JSON.parse(message.content);
@@ -117,15 +130,17 @@ export const cli = () => {
 
       let userResponse;
       do {
-        const response = await inquirer.prompt([{
-          type: 'list',
-          name: 'userResponse',
-          message: 'Do you wanna run this command?',
-          choices: ['Yes', 'No']
-        }]);
+        const response = await inquirer.prompt([
+          {
+            type: "list",
+            name: "userResponse",
+            message: "Do you wanna run this command?",
+            choices: ["Yes", "No"],
+          },
+        ]);
         userResponse = response.userResponse;
 
-        if (userResponse === 'Yes') {
+        if (userResponse === "Yes") {
           exec(output.command, (error, stdout, stderr) => {
             if (error) {
               return console.error(error.message);
@@ -136,8 +151,8 @@ export const cli = () => {
             console.log(stdout);
           });
         }
-      } while (userResponse === 'Explain');
+      } while (userResponse === "Explain");
     });
 
   program.parse();
-}
+};
